c0e1516e500e5a0eebd42b37d4db28e49da85af1
Decho Dechev - Progress <ddechev@progress.com>
2018-02-11 17:13:10 +0200

    Moved some mergebot handling to separate class

---
+++ Utilities/MergeBotHandler.cs
@@ -0,0 +1,51 @@
+﻿namespace ReinventionBot.Utilities
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Connector;
+
+    public static class MergeBotHandler
+    {
+        public static async Task HandleMergeBotMessage(Activity activity)
+        {
+            try
+            {
+                var prs = activity.Properties["pullrequests"];
+
+                if (prs.HasValues)
+                {
+                    await MessageComposer.SendMessageToUser("29:1Zg8q4E-_qtiNekcsFmWdbaih5zkCNlg4hbxMXwRFO2gbpaaLJTNDd-ifz5NYV30WV1NWvHL1lHPz-Wzg6wNt8A", "a:1FF_jJrHwxIBqzPMx4yLVjtoCVcD6o5Dkh7xqetx-rYDHSxmSmtPYMHsvC3W_qBkEVHVDOd9sewyfFYdYvGtW7kTKLQjUq7r3Cdhc5RcIqYGQbradXpl9i1mMAxPNroUD", "Merge bot performed some work.");
+
+
+                    foreach (var pr in prs)
+                    {
+                        var name = (string)pr.SelectToken("name");
+                        var author = (string)pr.SelectToken("author");
+                        var merged = (bool)pr.SelectToken("merged");
+                        var updated = (bool)pr.SelectToken("updated");
+
+                        string composedMessage = $"{name} form {author} is merged: {merged} and updated: {updated}";
+
+                        await MessageComposer.SendMessageToUser("29:1Zg8q4E-_qtiNekcsFmWdbaih5zkCNlg4hbxMXwRFO2gbpaaLJTNDd-ifz5NYV30WV1NWvHL1lHPz-Wzg6wNt8A", "a:1FF_jJrHwxIBqzPMx4yLVjtoCVcD6o5Dkh7xqetx-rYDHSxmSmtPYMHsvC3W_qBkEVHVDOd9sewyfFYdYvGtW7kTKLQjUq7r3Cdhc5RcIqYGQbradXpl9i1mMAxPNroUD", composedMessage);
+                    }
+                }
+                else
+                {
+                    await MessageComposer.SendMessageToUser("29:1Zg8q4E-_qtiNekcsFmWdbaih5zkCNlg4hbxMXwRFO2gbpaaLJTNDd-ifz5NYV30WV1NWvHL1lHPz-Wzg6wNt8A", "a:1FF_jJrHwxIBqzPMx4yLVjtoCVcD6o5Dkh7xqetx-rYDHSxmSmtPYMHsvC3W_qBkEVHVDOd9sewyfFYdYvGtW7kTKLQjUq7r3Cdhc5RcIqYGQbradXpl9i1mMAxPNroUD", "No pull request were merged or updated");
+                }
+            }
+            catch (Exception ex)
+            {
+                await MessageComposer.SendMessageToUser("29:1Zg8q4E-_qtiNekcsFmWdbaih5zkCNlg4hbxMXwRFO2gbpaaLJTNDd-ifz5NYV30WV1NWvHL1lHPz-Wzg6wNt8A", "a:1FF_jJrHwxIBqzPMx4yLVjtoCVcD6o5Dkh7xqetx-rYDHSxmSmtPYMHsvC3W_qBkEVHVDOd9sewyfFYdYvGtW7kTKLQjUq7r3Cdhc5RcIqYGQbradXpl9i1mMAxPNroUD", ex.ToString());
+
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                    await MessageComposer.SendMessageToUser("29:1Zg8q4E-_qtiNekcsFmWdbaih5zkCNlg4hbxMXwRFO2gbpaaLJTNDd-ifz5NYV30WV1NWvHL1lHPz-Wzg6wNt8A", "a:1FF_jJrHwxIBqzPMx4yLVjtoCVcD6o5Dkh7xqetx-rYDHSxmSmtPYMHsvC3W_qBkEVHVDOd9sewyfFYdYvGtW7kTKLQjUq7r3Cdhc5RcIqYGQbradXpl9i1mMAxPNroUD", ex.ToString());
+                }
+
+                throw new Exception();
+            }
+        }
+    }
+}
